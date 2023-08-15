5f6994f6e719feb5d17593794ce1cab79bda248c
Tim Montague <tim.john.montague@gmail.com>
2023-08-15 07:31:05 -0400

    got it dun this time

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using DECK_OF_CARDS_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DECK_OF_CARDS_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        //[HttpGet] //Default
+        public IActionResult Show5Cards()
+        {
+            
+            DeckModel result = DeckDAL.GetCards(5);
+            return View(result);
+        }
+        public IActionResult NewDeck()
+        {
+            DeckDAL.ResetCards();
+            return RedirectToAction("Show5Cards");
+        }
+        //[HttpPost] //form posts
+        //public IActionResult Show5Cards(bool nothing)
+        //{
+        //    DeckDAL.ResetCards();
+        //    DeckModel result = DeckDAL.GetCards(5);
+        //    return View(result);
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/DeckDAL.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace DECK_OF_CARDS_API.Models
+{
+    public class DeckDAL
+    {
+        public static DeckModel GetCards(int numberOfCards)//Adjust here
+        {
+            //adjust
+            //setup
+            string apiKey = Secret.apiKey;
+            string url = $"https://deckofcardsapi.com/api/deck/{apiKey}/draw/?count={numberOfCards}";
+
+            //request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Converting to json
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string json = reader.ReadToEnd();
+
+            //Adjust
+            //Convert to c#
+            //Install Newtonsoft.json
+            DeckModel result = JsonConvert.DeserializeObject<DeckModel>(json);
+            return result;
+        }
+        public static void ResetCards()//Adjust here
+        {
+            //adjust
+            //setup
+            string apiKey = Secret.apiKey;
+            string url = $"https://deckofcardsapi.com/api/deck/{apiKey}/shuffle/";
+
+            //request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Converting to json
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string json = reader.ReadToEnd();
+
+            //Adjust
+            //Convert to c#
+            //Install Newtonsoft.json
+            DeckModel result = JsonConvert.DeserializeObject<DeckModel>(json);
+            
+        }
+    }
+}
